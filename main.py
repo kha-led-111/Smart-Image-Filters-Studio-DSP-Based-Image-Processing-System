f4b92fe5e75af9e4cec8729c3be001191aa9c410
Khaled Gamal <khaledgmal644@gmail.com>
2026-05-22 18:45:02 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from UInew import AudioApp
-#from ui2 import AudioApp
-
-app = QApplication(sys.argv)
-window = AudioApp()
-window.show()
-sys.exit(app.exec_())
